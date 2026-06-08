109bf58881fef9a3a5edcdea797fc00896175881
Christian Hugo <hugo.christian@web.de>
2026-06-08 19:53:49 +0200

    app-constructor rules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const appConstructorRules = require("./app-constructor-rules");
+
 module.exports = {
   sc_plugin_api_version: 1,
   plugin_name: "many-to-many",
   viewtemplates: [require("./checkboxes")],
   ready_for_mobile: true,
+  app_constructor_rules: appConstructorRules.join("\n"),
 };
